1959711ed6ae1ef8ba96d9ec7e5853cedba10079
Zaky <mo.zaki@scbank.com.eg>
2025-08-07 10:39:02 +0300

    feat: initialize Angular application with routing and basic configuration

---
+++ apps/room-reservation/src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {providePrimeNG} from "primeng/config";
+import Aura from '@primeng/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark-mode',
+          cssLayer: false
+        }
+      }
+    }),
+    provideRouter(appRoutes),
+  ],
+};
